e86154d82b4479723b9f8b0513beb2955d65824a
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2023-09-11 01:24:59 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (71.97%) - LeetSync

---
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countOrders(int n) {
+        long long res=1,mod=1e9+7;
+        for(int i = 1 ; i <=n ;i++)
+        {
+            res*=i;
+            res%=mod;
+            res*=(2*i-1);
+            res%=mod;
+        }
+        return res%mod;
+    }
+};
