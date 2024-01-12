fb9ed31cb7471ac63b3a919dcb0048ec4634bbb2
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2024-01-12 14:12:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (46.92%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -7,15 +7,15 @@ public:
         return false;
     }
     bool halvesAreAlike(string s) {
-        int c1 = 0 , c2=0;
+        int c1 = 0;
         for(int i = 0 ; i < s.size()/2;i++)
         {
             if(checkV(s[i]))c1++;
         }
         for(int i = s.size()/2 ; i < s.size();i++)
         {
-            if(checkV(s[i]))c2++;
+            if(checkV(s[i]))c1--;
         }
-        return c1==c2;
+        return c1==0;
     }
 };
