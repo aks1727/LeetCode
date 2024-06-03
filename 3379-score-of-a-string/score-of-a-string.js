c0af9a99fcf4486a48f91ceca3d4a791ff1fe9f3
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2024-06-03 16:56:29 +0530

    Time: 64 ms (40.38%) | Memory: 49.6 MB (78.91%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function(s) {
+    let sum =0
+    for(let i = 1 ; i < s.length ;i++)
+    {   
+        sum = sum + Math.abs(s.charCodeAt(i) - s.charCodeAt(i-1));
+    }
+    return sum;
+};
