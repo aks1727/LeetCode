16350c57723aff720c349a590039e215a53a5d6e
Akshat Kumar Sinha <95994418+AKS1719@users.noreply.github.com>
2025-03-12 16:48:18 +0530

    Time: 4 ms (4.13%) | Memory: 27.2 MB (51.77%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin(),s.end());
+        solve(0,s.size()-1,s);
+        
+    }
+private:
+    void solve(int i ,int j, vector<char>&s){
+        if(i>=j)return;
+        swap(s[i],s[j]);
+        solve(i+1,j-1,s);
+
     }
 };
